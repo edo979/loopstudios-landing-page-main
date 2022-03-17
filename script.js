9b21de479f6258fe81eea93ff4c472756e7f9ec6
edo979 <prezentabilan@gmail.com>
2022-03-17 23:53:04 +0100

    mobile navigation finish

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const navbarEl = document.querySelector('.navbar'),
-  togglerEl = document.querySelector('.navbar-toggler')
+  togglerEl = document.querySelector('.navbar-toggler'),
+  closeBtn = document.querySelector('.navbar-close')
 
 togglerEl.addEventListener('click', (e) => navbarEl.classList.toggle('active'))
+
+closeBtn.addEventListener('click', (e) => navbarEl.classList.toggle('active'))
